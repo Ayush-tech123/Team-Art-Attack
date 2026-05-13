a68158b4378c172f6f7ac14b46b605ada7f80366
sanya raj <sanya.raj@avantika.edu.in>
2026-05-13 14:07:14 +0530

    Now key falling and scene 1 almost complete

--- Art_attack/sketch.js
+++ Art_attack/sketch.js
@@ -1,11 +1,13 @@
 let swing;
-let direction = 1;
+let dir = 1;
 let keyX = 240;
 let keyY = 245;
 let swim_speed = 0.5;
 let fishX = 75;
-let fishDirection = 1;
+let fishDir = 1;
 let hoveringKey = false;
+let falling = false;
+
 
 function setup() {
   createCanvas(600,500);
@@ -16,79 +18,77 @@ function draw() {
   background(217,175,182);
   strokeWeight(2);
   
-  //For Room 
+  //Room
   line(300,0,300,400);
-  fill("gray");
+  fill("grey")
   triangle(300,400,0,500,600,500);
   
   //Gate
   fill("burlywood")
   beginShape();
-  vertex(340,185);
-  vertex(340,412);
-  vertex(470,456);
-  vertex(470,217);
-  
+  vertex(340,185)
+  vertex(340,412)
+  vertex(470,456)
+  vertex(470,217)
   endShape(CLOSE);
   
   //DoorKnob
-  fill("black");
-  circle(460,340,10); 
+  fill("black")
+  circle(460,340,10)
   
   //Key Hanger
-  fill("wheat");
+  fill("wheat")
   beginShape();
-  vertex(200,218);
-  vertex(200,238);
-  vertex(250,228);
+  vertex(200,218)
+  vertex(200,238)
+  vertex(250,228)
   vertex(250,208)
   endShape(CLOSE);
   
-  line(240,225,240,240);
-  
-  hoveringKey = dist(mouseX, mouseY, 240, 245) < 20;
+  line(240,225,240,240)
+  hoveringKey = dist(mouseX, mouseY, keyX, keyY) < 20;
+
   drawKey()
-  
-  swing +=0.01 * direction;
-  
-  if(swing >=radians(135)) {
-    direction = -1;
+    if(!falling){   
+    swing += 0.01 * dir;
+  }
+
+  if (swing >= radians(135)) {
+    dir = -1;
   }
   if (swing <= radians(45)) {
-    direction = 1;
+    dir = 1;
   }
+  
   drawTable(-10, 410, 180, 80);
   drawFishTank(25, 350);
   
-  //Swimming logic
-  fishX += swim_speed * fishDirection;
-
-if (fishX > 100 || fishX < 50) {
-  fishDirection *= -1;
- }
-
+  fishX += swim_speed * fishDir;
+  if (fishX > 100 || fishX < 50) {
+    fishDir *= -1;
+  }
+  
+  if(falling){
+    keyY += 2;
+  }
 }
 
-
 function drawKey(){
-   
   push();
-  
-   translate(240,245);
-   rotate(swing);
-  
-//hover effect
+  translate(keyX, keyY);
+  rotate(swing);
+
+  // hover effect
   if(hoveringKey){
     stroke("green");
     strokeWeight(4);
     fill(220);
-  }
-  else{
+  } else {
     stroke(0);
     strokeWeight(2);
     fill("grey");
   }
-  
+
   // key ring
   circle(0,0,10);
 
@@ -98,7 +98,6 @@ function drawKey(){
   noFill();
   circle(5,0,5);
 
-  // key body
   fill(150);
   stroke(50);
   strokeWeight(1);
@@ -118,36 +117,29 @@ function drawKey(){
 
   pop();
 }
-//---------------------------------------
-function drawTable(x,y,w,h){
-  
+
+function drawTable(x, y, w, h) {
   push();
-  
   fill("saddlebrown");
   stroke(60);
   strokeWeight(2);
 
-  //tabletop
   beginShape();
   vertex(x, y);
   vertex(x + w, y - 50);
   vertex(x + w + 40, y - 20);
   vertex(x + 40, y + 30);
   endShape(CLOSE);
-  
-  //front side
+
   fill("peru");
-  
   beginShape();
   vertex(x + 40, y + 30);
   vertex(x + w + 40, y - 20);
   vertex(x + w + 40, y + h - 40);
   vertex(x + 40, y + h + 10);
   endShape(CLOSE);
-  
-  //left side 
+
   fill("sienna");
-  
   beginShape();
   vertex(x, y);
   vertex(x + 40, y + 30);
@@ -155,27 +147,21 @@ function drawTable(x,y,w,h){
   vertex(x, y + h);
   endShape(CLOSE);
   
-  //Drawers
   line(x + 40, y + 50, x + w + 40, y )
   circle(x + (w/2) + 40, y + 15, 5)
   circle(x + (w/2) + 40, y + 35, 5)
   circle(x + (w/2) + 40, y + 55, 5)
   line(x + 40, y + 70, x + w + 40, y + 20)
 
-  pop();   
-
+  pop();
 }
-//-----------------------------------------------------
-function drawFishTank(x,y){
-  
+
+function drawFishTank(x, y) {
   push();
-  
   stroke(40);
   strokeWeight(2);
 
-  //glass
   fill(120, 180, 220, 120);
-
   beginShape();
   vertex(x, y);
   vertex(x + 90, y - 25);
@@ -183,9 +169,7 @@ function drawFishTank(x,y){
   vertex(x + 30, y + 20);
   endShape(CLOSE);
 
-  // front glass
   fill(100, 170, 210, 100);
-
   beginShape();
   vertex(x + 30, y + 20);
   vertex(x + 120, y - 5);
@@ -194,7 +178,6 @@ function drawFishTank(x,y){
   endShape(CLOSE);
 
   fill(80, 150, 200, 90);
-
   beginShape();
   vertex(x, y);
   vertex(x + 30, y + 20);
@@ -202,15 +185,12 @@ function drawFishTank(x,y){
   vertex(x, y + 55);
   endShape(CLOSE);
 
-  // gravel
   noStroke();
   fill("tan");
-
   ellipse(x + 20, y + 52, 8);
   ellipse(x + 40, y + 58, 8);
   ellipse(x + 60, y + 54, 8);
 
-  // seaweed
   stroke("green");
   noFill();
 
@@ -221,32 +201,33 @@ function drawFishTank(x,y){
 
   beginShape();
   vertex(x + 70, y + 55);
-  bezierVertex(x + 65, y + 40,x + 78, y + 30, x + 72, y + 12);
-  
+  bezierVertex(x + 65, y + 40,x + 78, y + 30,x + 72, y + 12);
   endShape();
 
   push();
+  translate(x + fishX, y + 30);
 
-translate(x + fishX, y + 30);
+  if (fishDir < 0) {
+    scale(-1, 1);
+  }
 
-if (fishDirection < 0) {
-  scale(-1, 1);
+  noStroke();
+  fill("orange");
+
+  ellipse(0, 0, 18, 10);
+
+  triangle(-8, 0, -17, -5, -17, 5);
+
+  fill(0);
+  circle(5, -2, 2);
+
+  pop();
+  pop();
 }
 
-noStroke();
-fill("orange");
-
-// fish
-ellipse(0, 0, 18, 10);
-
-// tail 
-triangle(-8, 0,-17, -5,-17, 5);
-
-// eye
-fill(0);
-circle(5, -2, 2);
-
-pop();
-  pop();
-  
+function mousePressed() {
+  let d = dist(mouseX, mouseY, keyX, keyY);
+  if(d < 15){
+    falling = true;
+  }
 }
