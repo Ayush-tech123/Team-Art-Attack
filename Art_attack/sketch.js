b7e69555b1a4c56784f4544b8f8aaf1fd7384720
sanya raj <sanya.raj@avantika.edu.in>
2026-05-13 13:29:42 +0530

    Then I made a fish tank

--- Art_attack/sketch.js
+++ Art_attack/sketch.js
@@ -1,5 +1,8 @@
 let swing;
 let direction = 1;
+let swim_speed = 0.5;
+let fishX = 75;
+let fishDirection = 1;
 
 function setup() {
   createCanvas(600,500);
@@ -39,7 +42,7 @@ function draw() {
   
   line(240,225,240,240);
   
-  //---------------------------------------------
+//---------------------------------------------
   drawKey()
   
   swing +=0.01 * direction;
@@ -51,6 +54,14 @@ function draw() {
     direction = 1;
   }
   drawTable(-10, 410, 180, 80);
+  drawFishTank(25, 350);
+  
+  //Swimming logic
+  fishX += swim_speed * fishDirection;
+
+if (fishX > 100 || fishX < 50) {
+  fishDirection *= -1;
+   }
 }
 
 function drawKey(){
@@ -63,7 +74,7 @@ function drawKey(){
     strokeWeight(2);
     fill("grey");
   
-    // key ring
+  // key ring
   circle(0,0,10);
 
   fill(217,175,182);
@@ -92,7 +103,7 @@ function drawKey(){
 
   pop();
 }
-//------------------
+//---------------------------------------
 function drawTable(x,y,w,h){
   
   push();
@@ -134,6 +145,90 @@ function drawTable(x,y,w,h){
   circle(x + (w/2) + 40, y + 55, 5)
   line(x + 40, y + 70, x + w + 40, y + 20)
 
+  pop();   
+}
+//-----------------------------------------------------
+function drawFishTank(x,y){
+  
+  push();
+  
+  stroke(40);
+  strokeWeight(2);
+
+  //glass
+  fill(120, 180, 220, 120);
+
+  beginShape();
+  vertex(x, y);
+  vertex(x + 90, y - 25);
+  vertex(x + 120, y - 5);
+  vertex(x + 30, y + 20);
+  endShape(CLOSE);
+
+  // front glass
+  fill(100, 170, 210, 100);
+
+  beginShape();
+  vertex(x + 30, y + 20);
+  vertex(x + 120, y - 5);
+  vertex(x + 120, y + 50);
+  vertex(x + 30, y + 75);
+  endShape(CLOSE);
+
+  fill(80, 150, 200, 90);
+
+  beginShape();
+  vertex(x, y);
+  vertex(x + 30, y + 20);
+  vertex(x + 30, y + 75);
+  vertex(x, y + 55);
+  endShape(CLOSE);
+
+  // gravel
+  noStroke();
+  fill("tan");
+
+  ellipse(x + 20, y + 52, 8);
+  ellipse(x + 40, y + 58, 8);
+  ellipse(x + 60, y + 54, 8);
+
+  // seaweed
+  stroke("green");
+  noFill();
+
+  beginShape();
+  vertex(x + 35, y + 50);
+  bezierVertex(x + 30, y + 35,x + 45, y + 30,x + 38, y + 15);
+  endShape();
+
+  beginShape();
+  vertex(x + 70, y + 55);
+  bezierVertex(x + 65, y + 40,x + 78, y + 30, x + 72, y + 12);
+  
+  endShape();
+
+  push();
+
+translate(x + fishX, y + 30);
+
+if (fishDirection < 0) {
+  scale(-1, 1);
+}
+
+noStroke();
+fill("orange");
+
+// fish
+ellipse(0, 0, 18, 10);
+
+// tail 
+triangle(-8, 0,-17, -5,-17, 5);
+
+// eye
+fill(0);
+circle(5, -2, 2);
+
+pop();
   pop();
   
 }
